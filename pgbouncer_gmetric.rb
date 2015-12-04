4a36390272b8f230cee171e82c1ac0f3e49d40ec
Matt Casper <matthewvcasper@gmail.com>
2015-12-04 14:32:54 -0800

    Update the gmetric script to use more modern Ruby.

--- pgbouncer_gmetric.rb
+++ pgbouncer_gmetric.rb
@@ -3,12 +3,11 @@
 #
 # == Install Dependencies ==
 #
-# sudo apt-get install ruby ganglia-monitor 
+# sudo apt-get install ruby ganglia-monitor
 #
 # == Usage ==
 #
 # pgbouncer_gmetric.rb <databasename>
-#
 #
 # based heavily off of:
 # http://github.com/elecnix/postgres_gmetric
@@ -16,60 +15,64 @@
 # Released under the BSD License
 require 'optparse'
 
-(puts "FATAL: gmetric not found" ; exit 1) if !File.exists? "/usr/bin/gmetric"
+unless File.exists? '/usr/bin/gmetric'
+  abort "FATAL: gmetric not found"
+end
 
-$options = {}
-optparse = OptionParser.new do |opts|
+@port = 6432
+@user = ENV['LOGNAME']
+@verbose = false
+parser = OptionParser.new do |opts|
   opts.banner = "Usage: pgbouncer_gmetric.rb [-U <user>] <database>"
 
-  # Define the options, and what they do
-  $options[:verbose] = false
   opts.on( '-v', '--verbose', 'Output collected data' ) do
-    $options[:verbose] = true
+    @verbose = true
   end
 
-  $options[:user] = ENV['LOGNAME']
   opts.on( '-U', '--user USER', 'Connect as USER' ) do |user|
-    $options[:user] = user
+    @user = user
   end
 
-  $options[:port] = 6432
   opts.on( '-p', '--port PORT', 'Connect to PORT' ) do |port|
-    $options[:port] = port
+    @port = port
   end
-
 
   opts.on( '-h', '--help', 'Display this screen' ) do
     puts opts
     exit
   end
 end
-optparse.parse!
+parser.parse!
 
-$options[:database]=ARGV[0]
+# optparse pops off args as it processes them, leaving us with the database
+# as the first arg.
+@database = ARGV[0]
 
-(puts "Missing database"; exit 1) if $options[:database].empty?
-(puts "Missing user"; exit 1) if $options[:user].nil?
+if @database.nil?
+  abort 'Missing database, please supply it as the last argument.'
+elsif @user.nil?
+  abort 'Missing user, please supply it with the -U flag.'
+end
 
 def query(sql)
-  puts "psql -U #{$options[:user]} -p #{$options[:port]} -A -c \"#{sql}\" pgbouncer | grep  \"database\|#{$options[:database]}\" " if $options[:verbose]
-  `psql -U #{$options[:user]} -p #{$options[:port]} -A -c "#{sql}" pgbouncer | grep  "database\\\|#{$options[:database]}" `
+  if @verbose
+    puts "psql -U #{@user} -p #{@port} -A -c \"#{sql}\" pgbouncer | grep  \"database\|#{@database}\" "
+  end
+  `psql -U #{@user} -p #{@port} -A -c "#{sql}" pgbouncer | grep  "database\\\|#{@database}" `
 end
 
 def publish(sql)
-  data=query(sql)
-  lines=data.split("\n")
-  values=lines[1].split('|')
-  col=0
-  lines[0].split('|').each do |colname|
-    unless ( colname == "database" or colname == "user" )
-      v=values[col]
-      puts "#{colname}=#{v}" if $options[:verbose]
-     `gmetric --group pgbouncer --name "pgb_#{colname}" --value #{v} --type float --dmax=240`
+  lines = query(sql).split("\n")
+  values = lines[1].split('|')
+  lines[0].split('|').each_with_index do |colname, i|
+    unless colname == "database" || colname == "user"
+      v = values[i]
+      if @verbose
+        puts "#{colname}=#{v}"
+      end
+      `gmetric --group pgbouncer --name "pgb_#{colname}" --value #{v} --type float --dmax=240`
     end
-    col=col+1
   end
 end
 
 publish "show pools;"
-
